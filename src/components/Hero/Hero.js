653a6921074b369d7bff2007463901455732c5a5
Joel Bello <joel8bl@gmail.com>
2021-06-15 15:51:35 -0400

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,7 +13,12 @@ const Hero = (props) => (
           My Personal Portfolio
         </SectionTitle>
         <SectionText>
-        The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
+        Self-taught and quick adaptation to new environments.
+        Skilled in creating software projects from scratch to
+        production. Leadership and empathy. Proactive with
+        initiative and innovation. Habituated to teamwork, with a
+        high sense of responsibility. Ready to take new professional
+        challenges.
         </SectionText>
         <Button onClick={props.handleClick}>Learn More</Button>
       </LeftSection>
