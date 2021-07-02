7cd359a0e6025e0c2ece1f772f9230f21bfc087f
Joel Bello <joel8bl@gmail.com>
2021-07-02 19:52:37 -0400

    new project added

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,16 +1,26 @@
 export const projects = [
   {
-    title: 'HULU Clone',
-    description: "Using React.js, Next.js, Tailwind.css. The Web page is called Hulu Clone an Web App based on streaming.",
-      image: '/images/1.png',
-      tags: ['NextJS', 'ReactJS', 'Tailwind CSS'],
-    source: 'https://github.com/joelnbl/Hulu-2',
-    visit: 'https://hulu-2-smoky.vercel.app/',
+    title: "HULU Clone",
+    description:
+      "Using React.js, Next.js, Tailwind.css. The Web page is called Hulu Clone an Web App based on streaming.",
+    image: "/images/1.png",
+    tags: ["NextJS", "ReactJS", "Tailwind CSS"],
+    source: "https://github.com/joelnbl/Hulu-2",
+    visit: "https://hulu-2-smoky.vercel.app/",
     id: 0,
+  },
+  {
+    title: "Expense Tracker",
+    description:
+      "Using React.js to make an amazing app to track your expenses, one of the best funcionality of this app is that you can filter per month and gets a best experience of it.",
+    image: "/images/2.png",
+    tags: ["ReactJS"],
+    source: "https://github.com/joelnbl/Expense-Tracker",
+    id: 1,
   },
 ];
 
 export const TimeLineData = [
-  { year: 2020, text: 'Jr FullStack Developer', },
-  { year: 2014, text: 'FullStack Web Developer', },
+  { year: 2020, text: "FullStack Developer" },
+  { year: 2014, text: "FullStack Web Developer" },
 ];
