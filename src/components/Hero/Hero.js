0bba35ca4e496d94ae21cd292b7090e524d04381
Joel Bello <joel8bl@gmail.com>
2021-07-02 20:33:24 -0400

    fixing styles

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
   <>
@@ -13,14 +17,13 @@ const Hero = (props) => (
           My Personal Portfolio
         </SectionTitle>
         <SectionText>
-        Self-taught and quick adaptation to new environments.
-        Skilled in creating software projects from scratch to
-        production. Leadership and empathy. Proactive with
-        initiative and innovation. Habituated to teamwork, with a
-        high sense of responsibility. Ready to take new professional
-        challenges.
+          Self-taught and quick adaptation to new environments. Skilled in
+          creating software projects from scratch to production. Leadership and
+          empathy. Proactive with initiative and innovation. Habituated to
+          teamwork, with a high sense of responsibility. Ready to take new
+          professional challenges.
         </SectionText>
-        <Button onClick={props.handleClick}>Learn More</Button>
+        {/* <Button onClick={props.handleClick}>Learn More</Button> */}
       </LeftSection>
     </Section>
   </>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,14 @@
 export const projects = [
   {
+    title: "Expense Tracker",
+    description:
+      "Using React.js to make an amazing app to track your expenses, one of the best funcionality of this app is that you can filter per month and gets a best experience of it.",
+    image: "/images/2.png",
+    tags: ["ReactJS"],
+    visit: "https://github.com/joelnbl/Expense-Tracker",
+    id: 1,
+  },
+  {
     title: "HULU Clone",
     description:
       "Using React.js, Next.js, Tailwind.css. The Web page is called Hulu Clone an Web App based on streaming.",
@@ -8,15 +17,6 @@ export const projects = [
     source: "https://hulu-2-smoky.vercel.app/",
     visit: "https://github.com/joelnbl/Hulu-2",
     id: 0,
-  },
-  {
-    title: "Expense Tracker",
-    description:
-      "Using React.js to make an amazing app to track your expenses, one of the best funcionality of this app is that you can filter per month and gets a best experience of it.",
-    image: "/images/2.png",
-    tags: ["ReactJS"],
-    visit: "https://github.com/joelnbl/Expense-Tracker",
-    id: 1,
   },
 ];
 
